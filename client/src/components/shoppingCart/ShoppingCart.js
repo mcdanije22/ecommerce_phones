a3ea8a2ea32cf3953e894ae4845a220d232a07d8
mcdanije22 <mcdanije22@gmail.com>
2019-05-25 01:56:21 -0400

    ui for shopping cart

--- client/src/components/shoppingCart/ShoppingCart.js
+++ client/src/components/shoppingCart/ShoppingCart.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './shoppingcart.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes  } from '@fortawesome/free-solid-svg-icons';
 
 class ShoppingCart extends Component{
     constructor(){
@@ -8,18 +10,63 @@ class ShoppingCart extends Component{
    render(){
        return(
         <div id = 'shoppingCart'>
-        <ul>
+        <div id='cartHeading'>
+        <h1 >Shopping Cart</h1><p >Clear Cart</p>
+        </div>
+        <ul id='cartList'>
             <li className = 'cartItem'>
                 <img src='https://via.placeholder.com/80'/>
                 <div className='productDescription'>
+                    <button type = 'submit' className='cartDelete'>
+                        <FontAwesomeIcon icon={faTimes}/> 
+                    </button>
                     <h1>Iphone XS Max</h1>
                     <p>Apple</p>
                     <div className='bottomCard'>
-                    <button type='submit'>test</button>
-                    <p>75</p>
-                </div>
-                </div>
-                
+                        <div className ='quantityBtnGroup'>
+                            <button type='submit'>-</button>
+                            <input type='text'/>
+                            <button type='submit'>+</button>
+                        </div>
+                        <p><b>$1000</b></p>
+                    </div>
+                </div>   
+            </li>
+            <li className = 'cartItem'>
+                <img src='https://via.placeholder.com/80'/>
+                <div className='productDescription'>
+                    <button type = 'submit' className='cartDelete'>
+                        <FontAwesomeIcon icon={faTimes}/> 
+                    </button>
+                    <h1>Iphone XS Max</h1>
+                    <p>Apple</p>
+                    <div className='bottomCard'>
+                        <div className ='quantityBtnGroup'>
+                            <button type='submit'>-</button>
+                            <input type='text'/>
+                            <button type='submit'>+</button>
+                        </div>
+                        <p><b>$1000</b></p>
+                    </div>
+                </div>   
+            </li>
+            <li className = 'cartItem'>
+                <img src='https://via.placeholder.com/80'/>
+                <div className='productDescription'>
+                    <button type = 'submit' className='cartDelete'>
+                        <FontAwesomeIcon icon={faTimes}/> 
+                    </button>
+                    <h1>Iphone XS Max</h1>
+                    <p>Apple</p>
+                    <div className='bottomCard'>
+                        <div className ='quantityBtnGroup'>
+                            <button type='submit'>-</button>
+                            <input type='text'/>
+                            <button type='submit'>+</button>
+                        </div>
+                        <p><b>$1000</b></p>
+                    </div>
+                </div>   
             </li>
         </ul>
         </div>

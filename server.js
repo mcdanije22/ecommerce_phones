6a2bc7da55db4eb6c168efc4c1e072057f2a239e
mcdanije22 <mcdanije22@gmail.com>
2019-07-15 22:59:41 -0400

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Knex = require('Knex');
+// const Knex = require('Knex');
 
 // const db = Knex({
 //     client: 'pg',

1d4637980e9cc761e0859c78f98d3262869e5043
mcdanije22 <mcdanije22@gmail.com>
2019-07-16 21:15:15 -0400

    heroku

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const knex = require('knex');
 
 
 
-  const db = knex({
-    client: 'pg',
-    connection: {
-     connectionString: process.env.DATABASE_URL,
-     ssl:true,
-    }
-  });
+  // const db = knex({
+  //   client: 'pg',
+  //   connection: {
+  //    connectionString: process.env.DATABASE_URL,
+  //    ssl:true,
+  //   }
+  // });
   
 const app = express();
 const PORT = process.env.PORT || 3000;
